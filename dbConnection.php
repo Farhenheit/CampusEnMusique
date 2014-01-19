b54149c4d2cf30a5ee3379043f2497933ad332ab
vincentVanhecke <vincent.vanhecke@laposte.net>
2014-01-19 12:20:43 +0100

    second commit

--- dbConnection.php
+++ dbConnection.php
@@ -2,9 +2,7 @@
 
 // connection a la base de donnee
 	try {
-		$db = new PDO('mysql:host=mysql2.alwaysdata.com;dbname=campusenmusiqueorsay_miniblog', '52998', 'bvxt5u');
-		//$db = new PDO('mysql:host=mysql6.000webhost.com;dbname=a4115191_base', 'a4115191_user', 'cde34@5');
-		//$db = new PDO('mysql:host=localhost;dbname=miniblog', 'root', '');
+		$db = new PDO('mysql:host=localhost;dbname=miniblog', 'root', '');
 	} catch (Exception $e) {
 		die('erreur'.$e->getMessage());
 	}
